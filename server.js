a38c7262f2f21d0105449a0c6df9a8d7e698658f
cepholopoddreamz <leslie.plumb@gmail.com>
2019-06-22 00:13:12 +0000

    setting up server and upload enpoints

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require ('express')
+const fileUpload = require ('express-fileupload'
+);
+
+const PORT = 3001
+
+const app = express();
+
+app.use(fileUpload());
+
+//upload end point
+app.post(`/upload`, (req,res) =>{
+  if(req.files === null){
+    return res.status(400).json({ msg: 'no file was uploaded'})
+  }
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err){
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: file.name, filePath:`/uploads/${file.name}`})
+  });
+
+});
+
+
+app.listen(3001, ()=> console.log('now listening on Port' + PORT));
+
