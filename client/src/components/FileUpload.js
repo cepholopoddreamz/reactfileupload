689eafeca1d040b66aeb4acaf8c631bee3689e8b
cepholopoddreamz <leslie.plumb@gmail.com>
2019-07-05 05:41:26 +0000

    piggybacking off off the contedt alreay built for displaying  new image into the spinny thingy but ultimately i need to add a unique contedt that reads an array so i can keep adding images live

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,15 +1,21 @@
 import React, { Fragment, useState } from 'react'
 import axios from 'axios';
+import '../spiny.css';
+
+
+
+
 
 const FileUpload = () => {
 
-  const [ file, setFile] = useState('');
-  const [ filename, setFilename] = useState('Choose File');
+  const [ file, setFile ] = useState('');
+  const [ filename, setFilename] = useState('Choose File ');
   const [uploadedFile, setUploadedFile] = useState({})
   
   const onChange = e => {
     setFile(e.target.files[0]); // html can take in multiple files but we only want one - so setting the array at 0 fo 1 file
-    setFilename(e.target.files[0].name);
+    setFilename(e.target.files[0].name); //tried renaming here but this is just in the text area box
+    
   }
 
   const onSubmit = async e => {
@@ -30,6 +36,9 @@ const FileUpload = () => {
 
       setUploadedFile({ fileName, filePath });
 
+      //newFiles.push(filePath); //this is where i tested storing the file paths. but an array will only work on local storage and you can't requeszt a file list client side only -- so maybe a json object stored in mongodb
+      //console.log(newFiles)
+
     } catch(err){
       if(err.response.status === 500){
         console.log('There was a problem with the server')
@@ -40,6 +49,37 @@ const FileUpload = () => {
     }
 
   }
+
+  function createthingy(array){
+  
+    var spinspace = document.createElement('div');
+    spinspace.className = "spinspace";
+    var containerdiv = document.createElement('div');
+    containerdiv.className = "scene";
+    var plant = document.createElement('div');
+    plant.className = "plant";
+    for (var c in array) {  
+      var div = document.createElement('div');
+      div.className = "img";
+      containerdiv.appendChild(div)
+      var div2 = document.createElement('div');
+      div2.className = "img__content";
+      var myImage = new Image();
+      myImage.src = array[c]
+      div2.appendChild(myImage)
+      div.appendChild(div2)
+      plant.appendChild(div);
+      containerdiv.appendChild(plant);
+      spinspace.appendChild(containerdiv)
+      document.body.appendChild(spinspace)
+    }
+  }
+
+const checkarray = uploadedFile ? uploadedFile.filePath : ''
+
+let newFiles = [checkarray]
+  
+createthingy(newFiles)
 
   return (
     <Fragment>
